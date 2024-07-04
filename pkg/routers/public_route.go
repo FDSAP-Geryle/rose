37fc4f596c7972bb712acee42f889cf826163fdb
FDSAP-Geryle <111254926+FDSAP-Geryle@users.noreply.github.com>
2024-07-04 16:18:18 +0800

    Add files via upload

---
+++ pkg/routers/public_route.go
@@ -0,0 +1,50 @@
+package routers
+
+import (
+	controllers "rose/pkg/controllers"
+	"rose/pkg/controllers/healthchecks"
+	"rose/pkg/routers/routes"
+	"rose/pkg/utils/go-utils/encryptDecrypt"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func SetupPublicRoutes(app *fiber.App) {
+
+	// Endpoints
+	apiEndpoint := app.Group("/api")
+	publicEndpoint := apiEndpoint.Group("/public")
+	v1Endpoint := publicEndpoint.Group("/v1")
+
+	// Service health check
+	v1Endpoint.Get("/", healthchecks.CheckServiceHealth)
+
+	//MyTest routes
+	v1Endpoint.Get("/hello", func(c *fiber.Ctx) error { return c.SendString("hello mond") })
+	v1Endpoint.Get("/merchant_upload", routes.GetUploadMerchant)
+
+}
+
+func SetupPublicRoutesB(app *fiber.App) {
+	// Endpoints
+	apiEndpoint := app.Group("/api")
+	publicEndpoint := apiEndpoint.Group("/public")
+	v1Endpoint := publicEndpoint.Group("/v1")
+
+	// Service health check
+	v1Endpoint.Get("/", healthchecks.CheckServiceHealthB)
+
+	// Utility
+	utilityEndpoint := v1Endpoint.Group("utility")
+	utilityEndpoint.Post("/test/encrypt", encryptDecrypt.EncryptHandler)
+	utilityEndpoint.Post("/test/decrypt", encryptDecrypt.DecryptHandler)
+
+	// SMS
+	smsEndpoint := v1Endpoint.Group("sms")
+	smsEndpoint.Get("/test/get/all/sms/type", controllers.SMSBlastingContentType)
+
+	// ACTIVITY
+	activityEndpoint := v1Endpoint.Group("activity")
+	activityEndpoint.Post("/isPalindrome", controllers.PalindromeHandler)
+
+}
