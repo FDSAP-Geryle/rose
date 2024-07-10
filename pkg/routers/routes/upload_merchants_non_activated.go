9baf2d03c86f7e7138071901bcee7f865c2d1471
Reymond <mondy.23a@gmail.com>
2024-07-10 08:10:11 +0800

    Added Route upload_merchant_non_activated and ExcelParcer

--- pkg/routers/routes/upload_merchants_non_activated.go
+++ pkg/routers/routes/upload_merchants_non_activated.go
@@ -2,75 +2,27 @@ package routes
 
 import (
 	"fmt"
+	"os"
+	"rose/pkg/utils"
 
 	"github.com/gofiber/fiber/v2"
-	"github.com/xuri/excelize/v2"
 )
 
-func GetUploadMerchantNonActivated(c *fiber.Ctx) error {
-	return ParseExcel(c)
-}
-
-func ParseExcel(c *fiber.Ctx) error {
-	f, err := excelize.OpenFile("/Users/19-sdev-cmdi-rand/Downloads/downloadTemplate.xlsx")
+func UploadMerchantNonActivated(c *fiber.Ctx) error {
+	// Create Local ./upload directory
+	if _, err := os.Stat("./uploads"); os.IsNotExist(err) {
+		os.Mkdir("./uploads", os.ModePerm)
+	}
+	// File upload connection to frontend "file"
+	file, err := c.FormFile("file")
 	if err != nil {
-		fmt.Println(err)
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			"message": "Failed to open file",
-			"error":   err.Error(),
-		})
+		return err
 	}
-	defer func() {
-		if err := f.Close(); err != nil {
-			fmt.Println(err)
-		}
-	}()
-
-	// Get all sheet names
-	sheetNames := f.GetSheetList()
-	if len(sheetNames) == 0 {
-		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-			"message": "No sheets found in the Excel file",
-		})
+	// Save the file to the server/Local
+	filePath := fmt.Sprintf("./uploads/%s", file.Filename)
+	if err := c.SaveFile(file, filePath); err != nil {
+		return err
 	}
-
-	// Use the first sheet name
-	sheetName := sheetNames[0]
-
-	// Get all the rows in the first sheet
-	rows, err := f.GetRows(sheetName)
-	if err != nil {
-		fmt.Println(err)
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			"message": "Failed to get rows",
-			"error":   err.Error(),
-		})
-	}
-
-	if len(rows) < 2 {
-		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-			"message": "No data found in the sheet",
-		})
-	}
-
-	// Initialize a slice to hold the data
-	var data []map[string]string
-	headers := rows[0]
-
-	// Iterate over rows starting from the second row (index 1)
-	for _, row := range rows[1:] {
-		rowData := make(map[string]string)
-		for i, cell := range row {
-			if i < len(headers) {
-				header := headers[i]
-				rowData[header] = cell
-			}
-		}
-		data = append(data, rowData)
-	}
-
-	return c.Status(fiber.StatusOK).JSON(fiber.Map{
-		"message": "success",
-		"data":    data,
-	})
+	// Call ParserExel
+	return utils.ParseExcel(c, filePath)
 }
