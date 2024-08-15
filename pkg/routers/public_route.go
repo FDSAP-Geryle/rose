8ce3dc6fa343e0207d3e746845bce5986119bd86
Reymond <mondy.23a@gmail.com>
2024-08-15 16:49:20 +0800

    fully merge

--- pkg/routers/public_route.go
+++ pkg/routers/public_route.go
@@ -18,31 +18,5 @@ func SetupPublicRoutes(app *fiber.App) {
 
 	//MyTest routes
 	v1Endpoint.Get("/hello", func(c *fiber.Ctx) error { return c.SendString("hello mond") })
-	v1Endpoint.Get("/merchant_upload", routes.GetUploadMerchant)
-	v1Endpoint.Post("/merchant_upload_non_activated", routes.UploadMerchantNonActivated)
-
-}
-
-func SetupPublicRoutesB(app *fiber.App) {
-	// Endpoints
-	apiEndpoint := app.Group("/api")
-	publicEndpoint := apiEndpoint.Group("/public")
-	v1Endpoint := publicEndpoint.Group("/v1")
-
-	// Service health check
-	v1Endpoint.Get("/", healthchecks.CheckServiceHealthB)
-
-	// Utility
-	utilityEndpoint := v1Endpoint.Group("utility")
-	utilityEndpoint.Post("/test/encrypt", encryptDecrypt.EncryptHandler)
-	utilityEndpoint.Post("/test/decrypt", encryptDecrypt.DecryptHandler)
-
-	// SMS
-	smsEndpoint := v1Endpoint.Group("sms")
-	smsEndpoint.Get("/test/get/all/sms/type", controllers.SMSBlastingContentType)
-
-	// ACTIVITY
-	activityEndpoint := v1Endpoint.Group("activity")
-	activityEndpoint.Post("/isPalindrome", controllers.PalindromeHandler)
 
 }
